dff2098051665ccbd356d00489fe8245bf22fbd9
Nathan Bailey <dbailey@k12k.com>
2020-09-15 10:49:28 -0400

    Add login screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./app/screens/Main";
+import Login from "./app/screens/Login";
 
-const App = () => <Main />;
+const App = () => <Login />;
 
 export default App;
---
+++ app/screens/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Image,
+  Text,
+  Button,
+  View,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
+
+export default Login;
