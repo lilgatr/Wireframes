19d7e104f518441c6ecdf0c94a0ff6c7c33bffc1
Eli Salyer <mad@dbcs-a.k12k.com>
2020-09-25 11:33:17 -0400

    fixed styles error

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
-const Item = ({ title, body }) => (
+const ListItem = ({ title, body }) => (
   <View style={styles.item}>
     <Text style={styles.title}>{title}</Text>
     <Text>{body}</Text>
   </View>
 );
 
-export default Item;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "tomato",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default ListItem;
--- app/screens/List.js
+++ app/screens/List.js
@@ -8,17 +8,19 @@ import {
   StatusBar,
 } from "react-native";
 
-import Item from "../components/ListItem";
+import ListItem from "../components/ListItem";
 import DATA from "../utils/Data";
 
 export default () => {
-  const renderItem = ({ item }) => <Item title={item.title} />;
+  const renderListItem = ({ item }) => (
+    <ListItem title={item.title} body={item.body} />
+  );
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={DATA}
-        renderItem={renderItem}
+        renderItem={renderListItem}
         keyExtractor={(item) => item.id}
       />
     </SafeAreaView>
